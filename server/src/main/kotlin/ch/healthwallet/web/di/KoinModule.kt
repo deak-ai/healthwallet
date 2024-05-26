c753bb6dc6f5dd965dc46027c14f6df871eaf4a8
cto-deak-ai <cto@deak.ai>
2024-05-26 11:09:05 +0200

    adapted waltid repo config

--- server/src/main/kotlin/ch/healthwallet/web/di/KoinModule.kt
+++ server/src/main/kotlin/ch/healthwallet/web/di/KoinModule.kt
@@ -4,12 +4,15 @@ import ch.healthwallet.db.PisDbRepository
 import ch.healthwallet.db.PisDbRepositoryImpl
 import ch.healthwallet.repo.WaltIdIssuerRepository
 import ch.healthwallet.repo.WaltIdIssuerRepositoryImpl
+import ch.healthwallet.repo.WaltIdPrefs
 import ch.healthwallet.vc.PrescriptionVCIssuanceService
 import ch.healthwallet.vc.PrescriptionVCIssuanceServiceImpl
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.cookies.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.config.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.serialization.json.Json
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -30,10 +33,13 @@ fun koinModule(config: ApplicationConfig) = module {
         }
     }
 
+
     val issuerBaseUrl = config.property("services.issuer").getString()
 
+    val appPrefs = MutableStateFlow(WaltIdPrefs(waltIdWalletApi = issuerBaseUrl)).asStateFlow()
+
     single<WaltIdIssuerRepository> {
-        WaltIdIssuerRepositoryImpl(get(), issuerBaseUrl)
+        WaltIdIssuerRepositoryImpl(get(), appPrefs)
     }
 
     single<PrescriptionVCIssuanceService> {
